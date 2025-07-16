d0702bbf9e155e9e5b0a4e4506ca1b0ecc122324
m=0 <54184361+ya1-oa@users.noreply.github.com>
2025-07-16 08:21:24 -0700

    Update models with inspection fee.py

--- docsAppR/models.py
+++ docsAppR/models.py
@@ -331,7 +331,8 @@ class Landlord(models.Model):
     default_max_occupants = models.PositiveSmallIntegerField(default=2)
     default_parking_spaces = models.PositiveSmallIntegerField(default=2)
     default_parking_fee = models.PositiveSmallIntegerField(default=0)
-
+    default_inspection_fee = models.DecimalField(max_digits=10, decimal_places=2, default=300.00)
+    
     contact_person_1 = models.CharField(
         max_length=100, 
         verbose_name="Primary Contact Person",
