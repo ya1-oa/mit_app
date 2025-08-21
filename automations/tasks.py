076e8f13333fdbd9f36608b80e2015100c24daad
m=0 <54184361+ya1-oa@users.noreply.github.com>
2025-08-21 01:59:25 -0700

    Update tasks.py turned off headless mode

--- automations/tasks.py
+++ automations/tasks.py
@@ -16,7 +16,7 @@ from selenium.common.exceptions import (
 from selenium.webdriver.common.keys import Keys
 
 class AutomationTasks:
-    def __init__(self, browser: str = "chrome", headless: bool = True):
+    def __init__(self, browser: str = "chrome", headless: bool = false):
         self.automator = WebAutomator(browser=browser, headless=headless)
     
     def generic_automation(
@@ -711,3 +711,4 @@ class RoomTemplateAutomation:
             
 
         return results
+
